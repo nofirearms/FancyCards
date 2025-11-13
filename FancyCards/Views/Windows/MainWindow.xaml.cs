2b8a2377b41d4a0da6715bafe336f0d67c96a4cf
superuser <superuser@DESKTOP-ORUKUJ9>
2025-11-13 17:37:27 +0300

    Add project files.

---
+++ FancyCards/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace FancyCards.Views.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
