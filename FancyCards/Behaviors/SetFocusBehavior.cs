d1546a54d7750b711d9c43b5ba81bdc5998b3eb5
nofirearms <enter7heninja@outlook.com>
2026-02-25 04:42:19 +0300

    minor fixes

---
+++ FancyCards/Behaviors/SetFocusBehavior.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace FancyCards.Behaviors
+{
+    public class SetFocusBehavior : Behavior<FrameworkElement>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            AssociatedObject.Loaded -= OnLoaded;
+            AssociatedObject.Focus();
+        }
+    }
+}
